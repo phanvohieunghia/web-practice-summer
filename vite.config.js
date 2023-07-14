62e65d0042e3d5afc242ce05b721e0805f3467cf
phanvohieunghia <phvohieunghia@gmail.com>
2023-07-14 23:08:51 +0700

    feat: setup project

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 // const fileNames = ["src", "components", "assets", "page", "router"];
 // const filePaths = fileNames.reduce((acc, cur) => ({
 //   ...acc,
 //   [cur]: `/${cur === "src" ? cur : "src/" + cur}`,
 // }));
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    // alias: {
-    //   ...filePaths,
-    // },
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+	plugins: [react()],
+	resolve: {
+		alias: [{ find: '@', replacement: '/src' }],
+	},
 });
 3;
